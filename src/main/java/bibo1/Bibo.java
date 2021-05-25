eb5365368e03500770ff756e499f80730fceef10
Bibi <huitzilopochtlik@gmail.com>
2021-05-25 15:53:58 +0200

    bibo1

--- src/main/java/bibo1/Bibo.java
+++ src/main/java/bibo1/Bibo.java
@@ -1,7 +1,25 @@
 package bibo1;
 
 public class Bibo {
+
     public static void main(String[] args) {
-        System.out.println("Hello Bibo");
+
+        Integer[] a = new Integer[3];
+
+        Character[] b = new Character[3];
+
+        String s = "Hello";
+
+        char[] cs = s.toCharArray();
+
+        if(!Character.isDigit(cs[1])){
+            System.out.println("Sí");
+        }
+
+        for(Character c : s.toCharArray()){
+            if(Character.isDigit(c));
+        }
     }
 }
+
+
